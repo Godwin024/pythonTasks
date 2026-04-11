0d2637e86c24cb9b47218e0849c7fd430c39959f
Godwin024 <simondavid5220@gmail.com>
2026-04-11 18:30:21 +0100

    task in progress

---
+++ Algorithm/makingcupoftea.py
@@ -0,0 +1,18 @@
+#steps for making a cup of tea.
+
+steps : pour some water into the kettle.
+      : Turn on your gas and heat your kettle
+      : 
+      : Loop
+      : After some minutes check if the water is boiling
+      : If it us not boiling wait for another minutes
+      : Continue checking until the water boils
+      :
+      : Put your tea bag in a cup and pour in your  hot water
+      : let your tea soak a minute
+      :
+      : if
+      : If you like add add milk else skip
+      : If you like add sugar else skip
+      : remove tea bag,
+      : stir your tea and drink
