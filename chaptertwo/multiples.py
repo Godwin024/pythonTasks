3438d9d8ff5c8667bc88de912eb47886267ebbaf
Godwin024 <simondavid5220@gmail.com>
2026-04-11 12:22:02 +0100

    task

---
+++ chaptertwo/multiples.py
@@ -0,0 +1,10 @@
+the_number = 1024
+second_number = 10
+
+if the_number %4 == 0  and second_number %2 == 0:
+   print("it is a multiple of 4 ", "it is a multiply of 2")
+else:
+   print("it is not a multiple of 4", "it is not a multiple of 2")
+
+
+
