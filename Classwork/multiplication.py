d0fa4e449aa06384fe284e67540a22c2deb2de6e
Godwin024 <simondavid5220@gmail.com>
2026-04-20 10:45:08 +0100

    Multiplication number

---
+++ Classwork/multiplication.py
@@ -0,0 +1,13 @@
+multiple = int(input("Enter your digit number: "))
+
+counter = 0
+multiplication_number = 0
+
+for range in range(1, 11,):
+
+    count = range 
+    multiplication_number = range * multiple
+
+    print(multiple, 'X ', range,"= ",multiplication_number )
+
+    
